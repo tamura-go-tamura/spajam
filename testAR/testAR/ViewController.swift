6a3d8729c2443f7abaa184d8a4388f72d8f4bfea
Yuta Tamura <tamura.go.tamura@gmail.com>
2020-10-18 07:04:54 +0900

    だいたい完成したお！

--- testAR/testAR/ViewController.swift
+++ testAR/testAR/ViewController.swift
@@ -8,15 +8,38 @@
 import UIKit
 import SceneKit
 import ARKit
+// AVクラスをインポートする
+import AVFoundation
 
-var collideCount: Int = 0
 
 class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDelegate {
+    
+    
+    //鬼にぶつけた回数
+    public var collideCount: Int = 0
+    public var CountLabel:UILabel!
+    public var Title:UILabel!
+
 
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Title = UILabel(frame: CGRect(x:0 ,y:80, width: Int(self.view.frame.width), height: 40))
+        Title.text = "Majinko Mamemaki"
+        Title.textAlignment = NSTextAlignment.center
+        Title.textColor = UIColor.red
+        Title.font = UIFont(name: "Copperplate-Bold", size: 30)
+        self.view.addSubview(Title)
+        CountLabel = UILabel(frame: CGRect(x:Int(self.view.frame.width)/2-100 ,y:120, width: 200, height: 45))
+        CountLabel.text = String(collideCount) + "/5"
+        CountLabel.textAlignment = NSTextAlignment.center
+        CountLabel.font = UIFont.boldSystemFont(ofSize: 35.0)
+        //CountLabel.backgroundColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 0.6)
+        CountLabel.textColor = UIColor.white
+        self.view.addSubview(CountLabel)
+
         
         sceneView.scene.physicsWorld.contactDelegate = self
         
@@ -39,32 +62,34 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
          configuration.planeDetection = [.horizontal, .vertical]
         
         sceneView.session.run(configuration)
+        oniComing()
          }
     
     //鬼と衝突したらカウントアップ
     func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
             let nodeA = contact.nodeA
             let nodeB = contact.nodeB
-            if (nodeA.name == "mato" && nodeB.name == "tama") {
+            if (nodeA.name == "oni" && nodeB.name == "tama") {
                 collideCount += 1
                 nodeA.removeFromParentNode()
-            } else if (nodeB.name == "mato" && nodeA.name == "tama") {
+            } else if (nodeB.name == "oni" && nodeA.name == "tama") {
                 collideCount += 1
                 nodeB.removeFromParentNode()
             }
-        print(collideCount)
+        scorealert()
+        oniComing()
         }
 
          // 球を追加する
-    func addSphere(hitResult: ARHitTestResult) {
+    func addSphere() {
          // ノードの生成
-         let spherelNode = SCNNode()
+        let boxNode = SCNNode()
+        boxNode.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        let scene = SCNScene(named: "art.scnassets/beans.scn")
+        let spherelNode = (scene?.rootNode.childNode(withName: "Cube", recursively: false))!
         spherelNode.name = "tama"
-
+        spherelNode.rotation = SCNVector4(1, 0, 0, 0.25 * Float.pi)
          // Geometryと Transform の設定
-         let sphereGeometry = SCNSphere(radius: 0.03);
-         spherelNode.geometry = sphereGeometry
-         spherelNode.position = SCNVector3(hitResult.worldTransform.columns.3.x,hitResult.worldTransform.columns.3.y + 0.05,hitResult.worldTransform.columns.3.z)
 
          // PhysicsBody の挙動設定
          spherelNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape:
@@ -85,7 +110,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
             }
         spherelNode.position = camera.position
 
-        let targetPosCamera = SCNVector3Make(0, 0, -2)
+        let targetPosCamera = SCNVector3Make(0, 0, -3)
         let target = camera.convertPosition(targetPosCamera, to: nil)
         
          // 上向きの力をかける
@@ -96,6 +121,32 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
          sceneView.scene.rootNode.addChildNode(spherelNode)
         
     }
+
+    //アプリがらかれた時に誕生する！
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+            // Create a session configur
+        guard let camera = sceneView.pointOfView else {
+            return
+        }
+        let boxNode = SCNNode()
+        boxNode.geometry = SCNBox(width: 0.3, height: 0.3, length: 0.3, chamferRadius: 0)
+        let scene = SCNScene(named: "art.scnassets/oni.scn")
+        let rabbitNode = (scene?.rootNode.childNode(withName: "Cube", recursively: false))!
+        let cameraPos = SCNVector3Make(0, 0, -1)
+        let position = camera.convertPosition(cameraPos, to: nil)
+        //boxNode.position = position
+        rabbitNode.position = position
+        rabbitNode.name = "oni"
+        rabbitNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        rabbitNode.physicsBody?.isAffectedByGravity = false
+        rabbitNode.physicsBody?.categoryBitMask = 2
+        rabbitNode.physicsBody?.collisionBitMask = 1
+        rabbitNode.physicsBody?.contactTestBitMask = 1
+        sceneView.scene.rootNode.addChildNode(rabbitNode)
+    }
+
+    
 
           //平面が検出されたときに呼ばれる
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode,
@@ -132,26 +183,29 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
          // 検出したアンカーに対応するノードに子ノードとして持たせる
          node.addChildNode(planeNode)
         
-        
         ///ここ鬼にしてみよう
-        let node = SCNNode() // ノードを生成
-        node.geometry = SCNBox(width: 0.5, height: 0.5, length: 0.5, chamferRadius: 0) // 一片が10cmのキューブ
-        node.name = "mato"
-        let material = SCNMaterial() // マテリアル（表面）を生成する
-        // 表面の色は、ランダムで指定する
-        node.geometry?.materials = [material] // 表面の情報をノードに適用
+        oniComing()
+    }
 
-        node.position = SCNVector3(0, 0, -1.5) // ノードの位置は、原点から左右：0m 上下：0m　奥に50cmとする
-        node.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
-        node.physicsBody?.isAffectedByGravity = false
-        node.physicsBody?.categoryBitMask = 2
-        node.physicsBody?.collisionBitMask = 1
-        node.physicsBody?.contactTestBitMask = 1
-        sceneView.scene.rootNode.addChildNode(node) // 生成したノードをシーンに追加する
+    //鬼を召喚する
+    func oniComing(){
+        let boxNode = SCNNode()
+        boxNode.geometry = SCNBox(width: 0.3, height: 0.3, length: 0.3, chamferRadius: 0)
+        let scene = SCNScene(named: "art.scnassets/oni.scn")
+        let rabbitNode = (scene?.rootNode.childNode(withName: "Cube", recursively: false))!
+        rabbitNode.name = "oni"
+
+        rabbitNode.position = SCNVector3(Int.random(in: -3..<3), Int.random(in: -3..<3), Int.random(in: -3..<3)) // ノードの位置は、原点から左右：0m 上下：0m　奥に50cmとする
+        rabbitNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        rabbitNode.physicsBody?.isAffectedByGravity = false
+        rabbitNode.physicsBody?.categoryBitMask = 2
+        rabbitNode.physicsBody?.collisionBitMask = 1
+        rabbitNode.physicsBody?.contactTestBitMask = 1
+        sceneView.scene.rootNode.addChildNode(rabbitNode) // 生成したノードをシーンに追加する
     }
 
          // 平面が更新されたときに呼ばれる
-         func renderer(_ renderer: SCNSceneRenderer, didUpdate node:
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node:
         SCNNode, for anchor: ARAnchor) {
          guard let planeAnchor = anchor as? ARPlaneAnchor else
             
@@ -170,23 +224,23 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
     
     override func touchesBegan(_ touches: Set<UITouch>, with event:
     UIEvent?) {
-     // 最初にタップした座標を取り出す
-     guard let touch = touches.first else {return}
-
-     // スクリーン座標に変換する
-     let touchPos = touch.location(in: sceneView)
-
-     // 検出した平面との当たり判定
-     let hitTestResult = sceneView.hitTest(touchPos, types:.existingPlane)
-     
-     if !hitTestResult.isEmpty {
-     if let hitResult = hitTestResult.first {
-     // 平面とあたっていたら球を追加する
-     addSphere(hitResult :hitResult)
+        addSphere()
      }
-     }
-     }
+    
+    func scorealert() -> Void {
+            DispatchQueue.main.sync {
+            if self.collideCount == 5{
+                    let alert: UIAlertController = UIAlertController(title: "CLEAR", message: "おめでとうございます", preferredStyle:  UIAlertController.Style.alert)
+                let confirmAction = UIAlertAction(title: "閉じる", style:UIAlertAction.Style.cancel, handler: nil)
+                alert.addAction(confirmAction)
+                present(alert, animated: true, completion: nil)
+                    collideCount = 0
+                }
+                CountLabel.text = String(collideCount) + "/5"
+            }
     }
+    
+
         
                                                 
     // MARK: - ARSCNViewDelegate
@@ -199,7 +253,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
         return node
     }
 */
-    
+
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
         
@@ -215,3 +269,4 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
         
     }
 
+}

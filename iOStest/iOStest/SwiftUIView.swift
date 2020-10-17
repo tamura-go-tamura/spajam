0bd5f28c6071be664bc37374ccb2f905b87461a8
uni82 <mayu1368@gmail.com>
2020-10-17 21:40:28 +0900

    initial commit

---
+++ iOStest/iOStest/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  iOStest
+//
+//  Created by scsendai-000 on 2020/10/17.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}

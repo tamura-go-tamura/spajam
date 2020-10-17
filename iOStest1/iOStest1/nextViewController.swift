b092ff24eb26f27d4a74c479b632dec8dbdce361
uni82 <mayu1368@gmail.com>
2020-10-17 21:54:58 +0900

    initial commit

--- iOStest1/iOStest1/nextViewController.swift
+++ iOStest1/iOStest1/nextViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class nextViewController: UIViewController {
+    
+    var testtext:String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+        var y:Int  = 100
+        
+        var testlabel:UILabel = UILabel(frame: CGRect(x: 30, y: y, width: Int(self.view.bounds.width), height: 50))
+        
+        testlabel.text = testtext
+        
+        self.view.addSubview(testlabel)
+        
+        
+        
+    
         // Do any additional setup after loading the view.
     }
     
--- testAR/testAR/ViewController.swift
+++ testAR/testAR/ViewController.swift
@@ -27,25 +27,112 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the scene to the view
         sceneView.scene = scene
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        // 特徴点を描画
+         sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
 
-        // Run the view's session
+         // ライトの追加
+         sceneView.autoenablesDefaultLighting = true;
+
+         // 平面を検出
+         let configuration = ARWorldTrackingConfiguration()
+         configuration.planeDetection = .horizontal
+        
         sceneView.session.run(configuration)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
+         }
+
+         // 球を追加する
+    func addSphere(hitResult: ARHitTestResult) {
+         // ノードの生成
+         let spherelNode = SCNNode()
+
+         // Geometryと Transform の設定
+         let sphereGeometry = SCNSphere(radius: 0.03);
+         spherelNode.geometry = sphereGeometry
+         spherelNode.position = SCNVector3(hitResult.worldTransform.columns.3.x,hitResult.worldTransform.columns.3.y + 0.05,hitResult.worldTransform.columns.3.z)
+
+         // PhysicsBody の挙動設定
+         spherelNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape:
+        nil)
         
-        // Pause the view's session
-        sceneView.session.pause()
+        // PhysicsBody のパラメータ設定
+         spherelNode.physicsBody?.mass = 1.0
+         spherelNode.physicsBody?.friction = 1.5
+         spherelNode.physicsBody?.rollingFriction = 1.0
+         spherelNode.physicsBody?.damping = 0.5
+         spherelNode.physicsBody?.angularDamping = 0.5
+         spherelNode.physicsBody?.isAffectedByGravity = true
+
+         // 上向きの力をかける
+        spherelNode.physicsBody?.applyForce(SCNVector3(3,1.5,-3.0),
+        asImpulse: true)
+
+         // ノードの追加
+         sceneView.scene.rootNode.addChildNode(spherelNode)
+        
     }
 
+          //平面が検出されたときに呼ばれる
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode,
+        for anchor: ARAnchor) {
+         guard let planeAnchor = anchor as? ARPlaneAnchor else
+        {fatalError()}
+
+         // ノード作成
+         let planeNode = SCNNode()
+
+         // ジオメトリの作成する
+         let geometry = SCNPlane(width: CGFloat(planeAnchor.extent.x),
+         height: CGFloat(planeAnchor.extent.z))
+         geometry.materials.first?.diffuse.contents = UIColor.black.withAlphaComponent(0.5)
+
+         // ノードに Geometryと Transform を指定
+         planeNode.geometry = geometry
+         planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2.0, 1,
+        0, 0)
+
+         // 検出したアンカーに対応するノードに子ノードとして持たせる
+         node.addChildNode(planeNode)
+    }
+
+         // 平面が更新されたときに呼ばれる
+         func renderer(_ renderer: SCNSceneRenderer, didUpdate node:
+        SCNNode, for anchor: ARAnchor) {
+         guard let planeAnchor = anchor as? ARPlaneAnchor else
+            
+         {fatalError()}
+          guard let geometryPlaneNode = node.childNodes.first,
+          let planeGeometory = geometryPlaneNode.geometry as?
+         SCNPlane else {fatalError()}
+
+          // ジオメトリをアップデートする
+          planeGeometory.width = CGFloat(planeAnchor.extent.x)
+          planeGeometory.height = CGFloat(planeAnchor.extent.z)
+          geometryPlaneNode.simdPosition = float3(planeAnchor.center.x,0,planeAnchor.center.z)
+         }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event:
+    UIEvent?) {
+     // 最初にタップした座標を取り出す
+     guard let touch = touches.first else {return}
+
+     // スクリーン座標に変換する
+     let touchPos = touch.location(in: sceneView)
+        print(touchPos)
+
+     // 検出した平面との当たり判定
+     let hitTestResult = sceneView.hitTest(touchPos, types:.existingPlane)
+     print(hitTestResult)
+     if !hitTestResult.isEmpty {
+     if let hitResult = hitTestResult.first {
+     // 平面とあたっていたら球を追加する
+     addSphere(hitResult :hitResult)
+     }
+     }
+     }
+    }
+        
+                                                
     // MARK: - ARSCNViewDelegate
     
 /*
@@ -71,4 +158,4 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
-}
+
